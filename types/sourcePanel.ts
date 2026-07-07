3039cd241de16999fe02b188ca0f625fe7f2aa74
rrana-wmf <rrana-ctr@wikimedia.org>
2026-07-07 22:11:23 +0530

    feat: persist source panel expand/collapse state during session (TAL-319)

--- types/sourcePanel.ts
+++ types/sourcePanel.ts
@@ -2,4 +2,6 @@ import { Document } from "langchain/document";
 
 export interface SourcePanelProps {
     sources: Document[];
+    expandedSources: Set<number>;
+    onChange: (next: Set<number>) => void;
 }
